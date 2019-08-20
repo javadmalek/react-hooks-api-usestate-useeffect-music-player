f5bc5ee534bc52659b6cce38d0ad0ded3daac9a8
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-08-20 23:13:55 +0200

    [file_structure] changing the file structure

--- src/app.js.jsx
+++ src/app.js.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "./resources/logo.svg";
-import FetchTrending from "./hooks/fetch_trending.js.jsx";
 import Skeleton from "react-loading-skeleton";
 
+import FetchTrending from "./componsnts/fetch_trending.js.jsx";
 import Tracks from "./componsnts/tracks.js.jsx";
 
+import logo from "./resources/logo.svg";
 const URL_TRENDING = "https://api-stg.jam-community.com/song/trending";
 
 const app = () => {
