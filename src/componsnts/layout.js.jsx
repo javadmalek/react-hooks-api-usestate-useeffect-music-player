b35664b19255430a478175d66518f6ccbce22231
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-08-22 00:38:20 +0200

    [css][jsx] preparing the basic layouts and components

---
+++ src/componsnts/layout.js.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const FlexboxDiv = styled.div`
+    display: flex;
+    ${({ alignItems = "flex-start" }) => `align-items:${alignItems};`}
+    ${({ justifyContent = "flex-start" }) =>
+      `justify-content:${justifyContent};`}
+    ${({ flexDirection = "row" }) => `flex-direction:${flexDirection};`}
+    ${({ flexWrap = "nowrap" }) => `flex-wrap:${flexWrap};`}
+    ${({ flexGrow = 1 }) => `flex:${flexGrow};`}
+    ${({ bgUrl }) =>
+      bgUrl &&
+      `background: url("${bgUrl}") no-repeat center center ; background-size: contain; background-color: #eee;`}
+    ${({ marginRight }) => marginRight && "margin-right: 1em;"}
+    ${({ dimension1 }) =>
+      dimension1 &&
+      "min-height : 15em; max-height: 15em; min-width: 10em; max-width: 10em;"}
+    ${({ borderRadius }) => borderRadius && "border-radius: 5px;"}
+    ${({ w100 }) => w100 && "width: 100%;"}
+
+`;
+
+export const TextTitle = styled.span`
+  font-weight: 500;
+  ${({ paddingTBS }) => paddingTBS && "padding: 0.5em 0;"}
+`;
+export const TextSubtitle = styled.span`
+  font-size: 0.75rem;
+  ${({ paddingTBS }) => paddingTBS && "padding: 0.5em 0;"}
+`;
