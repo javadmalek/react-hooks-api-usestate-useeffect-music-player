3122d17b9263c486582c7250cc720a6112871178
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-08-25 21:49:40 +0200

    [css] responsive layout

--- src/componsnts/layout.js.jsx
+++ src/componsnts/layout.js.jsx
@@ -11,20 +11,21 @@ export const FlexboxDiv = styled.div`
     ${({ flexBasis }) => flexBasis && `flex-Basis:${flexBasis};`}
 
     ${({ borderColor }) =>
-      borderColor && `border-color: lighten(#34495E, 10%);`}
-    ${({ borderRadius }) => borderRadius && `border-radius: 0.4em;`}
-    ${({ backgroundDark }) => backgroundDark && `background: #34495E;`}
+      borderColor && "border-color: lighten(#34495E, 10%);"}
+    ${({ borderRadius }) => borderRadius && "border-radius: 0.4em;"}
+    ${({ backgroundDark }) => backgroundDark && "background: #34495E;"}
     ${({ bgUrl }) =>
       bgUrl &&
       `background: url("${bgUrl}") no-repeat center center ; background-size: contain; background-color: #eee;`}
     
-    ${({ colorGold }) => colorGold && `color: #dd5;`}
+    ${({ colorGold }) => colorGold && "color: #dd5;"}
     
+    ${({ marginReset }) => marginReset && "margin: 0;"}
     ${({ marginRight }) => marginRight && "margin-right: 1em;"}
     ${({ paddingDefault }) => paddingDefault && "padding: 1em;"}
     ${({ paddingTBDefault }) => paddingTBDefault && "padding: 2em 0;"}
     ${({ paddingTBS }) => paddingTBS && "padding: 0.5em 0;"}
-    ${({ paddingDefault }) => paddingDefault && "margin: 0 1em;"}
+    ${({ paddingDefault }) => paddingDefault && "padding: 0 1em;"}
 
     ${({ borderRadius }) => borderRadius && "border-radius: 5px;"}
     
@@ -35,8 +36,26 @@ export const FlexboxDiv = styled.div`
       ${({ dimension2 }) =>
         dimension2 &&
         "min-height : 15em; max-height: 15em; min-width: 15em; max-width: 15em;"}
+        ${({ dimension3 }) =>
+          dimension3 &&
+          "min-height : 5em; max-height: 5em; min-width: 5em; max-width: 5em;"}
     ${({ w100 }) => w100 && "width: 100%;"}
     ${({ h15e }) => h15e && "height: 15em;"}
+
+    @media (max-width: 480px) {
+      ${({ mobileFlexColumn }) => mobileFlexColumn && "flex-direction: column;"}
+      ${({ mobileAlignItems }) =>
+        mobileAlignItems && `align-items: ${mobileAlignItems};`}
+      ${({ mobileJustifyContent }) =>
+        mobileJustifyContent && `justify-content: ${mobileJustifyContent};`}
+      ${({ mobileDisplayNone }) => mobileDisplayNone && "display: none;"}
+      ${({ mobileDimension3 }) =>
+        mobileDimension3 &&
+        "min-height : 3em; max-height: 3em; min-width: 3em; max-width: 3em;"}
+      ${({ mobileWAuto }) => mobileWAuto && "width: auto;"}
+
+    }
+
 `;
 
 export const TextTitle = styled.span`
