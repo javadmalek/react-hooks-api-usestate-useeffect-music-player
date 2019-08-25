00cef3f7069bca979ff74336250718485b72d3a2
Javad Malekshahkoohi <mrjavad@gmail.com>
2019-08-25 22:02:22 +0200

    [jsx] [loading] skeletone

--- src/componsnts/highlight_track.js.jsx
+++ src/componsnts/highlight_track.js.jsx
@@ -7,6 +7,7 @@ import PostMessageForm from "./post_message_form.js";
 import ActionLikeIcon from "./action_icon_like.js";
 import { formatMSS } from "./helper";
 import IconLike from "../resources/icon_like.svg";
+import Skeleton from "react-loading-skeleton";
 
 const HighlightTrack = props => {
   const { track } = props;
@@ -33,24 +34,29 @@ const HighlightTrack = props => {
       <FlexboxDiv w100 flexDirection="column">
         <FlexboxDiv w100 flexDirection="column" mobileAlignItems="center">
           <TextTitle fontBold fontSizeXl lineHeightL>
-            {name}
+            {name || <Skeleton />}
           </TextTitle>
           <TextTitle fontSizeL>{artistName}</TextTitle>
-          <TextTitle fontSizeS lineHeightL>{`${formatMSS(
-            duration
-          )} / ${dateFormat(songRelease, "yyyy-mm-dd")}`}</TextTitle>
           <TextTitle fontSizeS lineHeightL>
-            {description}
+            {`${formatMSS(duration)} / ${dateFormat(
+              songRelease,
+              "yyyy-mm-dd"
+            )}` || <Skeleton />}
+          </TextTitle>
+          <TextTitle fontSizeS lineHeightL>
+            {description || <Skeleton />}
           </TextTitle>
         </FlexboxDiv>
         <FlexboxDiv w100 alignItems="center" mobileJustifyContent="center">
           <img src={IconLike} alt={`likes: ${likes}`} />
-          <TextTitle marginRightL lineHeightL>{` ${likes}`}</TextTitle>
+          <TextTitle marginRightL lineHeightL>{` ${likes || (
+            <Skeleton />
+          )}`}</TextTitle>
           <TextTitle marginRightL lineHeightL>
-            plays:{` ${plays}`}
+            plays:{` ${plays || <Skeleton />}`}
           </TextTitle>
           <TextTitle marginRightL lineHeightL>
-            faves:{` ${faves}`}
+            faves:{` ${faves || <Skeleton />}`}
           </TextTitle>
         </FlexboxDiv>
         <FlexboxDiv w100 alignItems="center" mobileJustifyContent="center">
